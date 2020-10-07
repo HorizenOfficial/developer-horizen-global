eef2e0b6369a14cba9a7885e7dfd9ac30ef9d4ac
zennermac <mac@zensystem.io>
2020-10-07 17:39:37 +0100

    hotfix for relative paths in local conf.py for docker live development

--- docs/local-config/conf.py
+++ docs/local-config/conf.py
@@ -130,28 +130,23 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None
-html_logo = 'images/Horizen_UBD_white.svg'
+html_logo = '../images/Horizen_UBD_white.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/Horizen_favicon_32x32.png'
+html_favicon = '../images/Horizen_favicon_32x32.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
 html_js_files = ['js/target_blank.js','js/expand_tabs.js']
 html_css_files = ['custom.css']
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
